9a55b1a99019651e799b916699767fc8ab2c1de4
MiskolcziLevente1 <levente.miskolczi@stud.uni-corvinus.hu>
2025-04-27 11:38:08 +0200

    Update HF2.sql

--- HF/HF2.sql
+++ HF/HF2.sql
@@ -1,17 +1,17 @@
 CREATE Table UGYFEL2
 (
-LOGIN nvarchar(100) MASKED WITH (Function = 'default()'),
-EMAIL nvarchar(100) MASKED WITH (Function = 'email()'),
-NEV nvarchar(100) MASKED WITH (Function = 'partial(1, "XXX", 1)'),
-SZULEV int MASKED WITH (Function = 'random(1900,2000)',
-NEM nvarchar(100),
-CIM nvarchar(100) MASKED WITH (Function = 'default()')
-);
+    LOGIN nvarchar(100) MASKED WITH (Function = 'default()'),
+    EMAIL nvarchar(100) MASKED WITH (Function = 'email()'),
+    NEV nvarchar(100) MASKED WITH (Function = 'partial(1, "XXX", 1)'),
+    SZULEV int MASKED WITH (Function = 'random(1900,2000)'),
+    NEM nvarchar(100),
+    CIM nvarchar(100) MASKED WITH (Function = 'default()')
+)
 
 INSERT INTO UGYFEL2
 (LOGIN,EMAIL,NEV,SZULEV,NEM,CIM)
 SELECT login, email, nev, szulev, nem, cim
-FROM Ugyfel;
+FROM Ugyfel
 
 CREATE USER MaskUser WITHOUT Login;
 GRANT SELECT ON UGYFEL2 TO MaskUser
